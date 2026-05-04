a8a6682c25d250f06854ec1b5755db1ea7f25286
jyin1003 <jyin1003@gmail.com>
2026-05-04 20:04:48 +1000

    update: nyc web link

--- datasets/datasets-data.js
+++ datasets/datasets-data.js
@@ -551,7 +551,7 @@ window.DATASETS_DATA = [
       "demographic",
       "geospatial"
     ],
-    "link": "https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2020-to-Present/erm2-nwe9",
+    "link": "https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2020-to-Present/erm2-nwe9/about_data",
     "dataset_schema": [
       {
         "label": "311 Service Request Records",
